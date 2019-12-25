cbb7e2a7c5d688697794e9bbe51ebb230b92a292
Samuel Gélineau <gelisam+github@gmail.com>
2019-12-24 19:56:45 -0500

    expand the test suite

--- test/Test.hs
+++ test/Test.hs
@@ -15,6 +15,26 @@ ex1b :: ((as ++ bs) ~ ((as ++ '[]) ++ (bs ++ '[])) => r)
      -> r
 ex1b r _ _ = r
 
+ex1c :: ((as ++ (bs ++ cs)) ~ (((as ++ '[]) ++ (bs ++ '[])) ++ (cs ++ '[])) => r)
+     -> proxy as
+     -> proxy bs
+     -> proxy cs
+     -> r
+ex1c r _ _ _ = r
+
+ex1d :: (proxy (as ++ bs) ~ proxy ((as ++ '[]) ++ (bs ++ '[])) => r)
+     -> proxy as
+     -> proxy bs
+     -> r
+ex1d r _ _ = r
+
+ex1e :: proxy as
+     -> proxy bs
+     -> proxy (as ++ bs)
+     -> proxy ((as ++ '[]) ++ (bs ++ '[]))
+ex1e _ _ r = r
+
+
 ex2a :: (((as ++ bs) ++ cs) ~ (as ++ (bs ++ cs)) => r)
      -> proxy as
      -> proxy bs
@@ -28,6 +48,28 @@ ex2b :: ((as ++ (bs ++ cs)) ~ ((as ++ bs) ++ cs) => r)
      -> proxy cs
      -> r
 ex2b r _ _ _ = r
+
+ex2c :: ((as ++ (bs ++ (cs ++ ds))) ~ (((as ++ bs) ++ cs) ++ ds) => r)
+     -> proxy as
+     -> proxy bs
+     -> proxy cs
+     -> proxy ds
+     -> r
+ex2c r _ _ _ _ = r
+
+ex2d :: (proxy (as ++ (bs ++ cs)) ~ proxy ((as ++ bs) ++ cs) => r)
+     -> proxy as
+     -> proxy bs
+     -> proxy cs
+     -> r
+ex2d r _ _ _ = r
+
+ex2e :: proxy as
+     -> proxy bs
+     -> proxy cs
+     -> proxy (as ++ (bs ++ cs))
+     -> proxy ((as ++ bs) ++ cs)
+ex2e _ _ _ r = r
 
 
 main :: IO ()
