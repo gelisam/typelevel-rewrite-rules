6e6be53a354487675952a28f5c9e8fe01ec0652d
Samuel Gélineau <gelisam+github@gmail.com>
2020-12-28 12:50:18 -0500

    rewrite the term-rewriting algorithm
    we were previously relying on the term-rewriting library, but now that
    we want to do some more complicated rewrites, we need to have our own
    implementation.

--- src/TypeLevel/Rewrite/Internal/TypeRule.hs
+++ src/TypeLevel/Rewrite/Internal/TypeRule.hs
@@ -1,5 +1,16 @@
 {-# LANGUAGE ViewPatterns #-}
+{-# OPTIONS -Wno-name-shadowing #-}
 module TypeLevel.Rewrite.Internal.TypeRule where
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.State
+import Data.Foldable (asum, for_)
+import Data.Map (Map)
+import Data.Maybe (isJust)
+import Data.Traversable
+import qualified Data.Map as Map
 
 -- GHC API
 import Name (getOccString)
@@ -7,9 +18,11 @@ import Type (TyVar, Type)
 
 -- term-rewriting API
 import Data.Rewriting.Rule (Rule(..))
-import Data.Rewriting.Rules (Reduct(result), fullRewrite)
-import Data.Rewriting.Term (Term(Fun))
+import Data.Rewriting.Substitution (gApply)
+import Data.Rewriting.Term (Term(..))
+import qualified Data.Rewriting.Substitution.Type as Substitution
 
+import TypeLevel.Rewrite.Internal.TypeEq
 import TypeLevel.Rewrite.Internal.TypeNode
 import TypeLevel.Rewrite.Internal.TypeTemplate
 import TypeLevel.Rewrite.Internal.TypeTerm
@@ -38,8 +51,79 @@ applyRules rules
     go 0 _
       = error "the rewrite rules form a cycle"
     go fuel typeTerm
-      = case fullRewrite rules typeTerm of
-          []
+      = case multiRewrite rules typeTerm of
+          Nothing
             -> typeTerm
-          reducts
-            -> go (fuel - 1) . result . last $ reducts
+          Just typeTerm'
+            -> go (fuel - 1) typeTerm'
+
+type Subst = Map TyVar (Term TypeNode TypeEq)
+
+multiRewrite
+  :: [TypeRule]
+  -> TypeTerm
+  -> Maybe TypeTerm
+multiRewrite rules input
+  = asum
+    [ singleRewrite rule input
+    | rule <- rules
+    ]
+
+-- >>> singleRewrite (F x (F x y) ~ F x y) [F a (F a b)]
+-- Just [F a b]
+singleRewrite
+  :: TypeRule
+  -> TypeTerm
+  -> Maybe TypeTerm
+singleRewrite rule input@(Fun inputF inputXS)
+    = topLevelRewrite rule input
+  <|> zipRewrite inputF inputXS (fmap (singleRewrite rule) inputXS)
+singleRewrite rule input
+  = topLevelRewrite rule input
+
+
+-- >>> topLevelRewrite (F x (F x y) ~ F x y) (F a (F a b))
+-- Just (F a b)
+topLevelRewrite
+  :: TypeRule
+  -> TypeTerm
+  -> Maybe TypeTerm
+topLevelRewrite (Rule pattern0 pattern') input0 = do
+  subst <- execStateT (go pattern0 input0) Map.empty
+  gApply (Substitution.fromMap subst) pattern'
+  where
+    go
+      :: Term TypeNode TyVar
+      -> TypeTerm
+      -> StateT Subst Maybe ()
+    go (Var var) input = do
+      subst <- get
+      case Map.lookup var subst of
+        Nothing -> do
+          modify (Map.insert var input)
+        Just term -> do
+          guard (input == term)
+    go (Fun patternF patternXS)
+       (Fun inputF inputXS)
+       = do
+      guard (patternF == inputF)
+      guard (length patternXS == length inputXS)
+      for_ (zip patternXS inputXS) $ \(pattern, input) -> do
+        go pattern input
+    go _ _ = do
+      lift Nothing
+
+-- >>> zipRewrite F [x,y,z] [Nothing,Nothing,Nothing]
+-- Nothing
+-- >>> zipRewrite F [x,y,z] [Just x',Nothing,Just z']
+-- Just [x',y,z']
+zipRewrite
+  :: TypeNode
+  -> [TypeTerm]
+  -> [Maybe TypeTerm]
+  -> Maybe TypeTerm
+zipRewrite f inputXS intermediateXS = do
+  guard (any isJust intermediateXS)
+  outputXS <- for (zip inputXS intermediateXS) $ \(input, intermediate) -> do
+    intermediate <|> pure input
+  pure $ Fun f outputXS
